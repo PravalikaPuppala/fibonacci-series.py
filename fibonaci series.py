c628dd2ee4ed58c94f1adbc23d4d612f75aca88c
PravalikaPuppala <71279224+PravalikaPuppala@users.noreply.github.com>
2020-09-17 20:38:10 +0530

    Add files via upload
    This display the fibonacci series of numbers in the range that is given as input

---
+++ fibonaci series.py
@@ -0,0 +1,15 @@
+j=int(input("enter the number where the series should:"))
+i=0
+a=0
+b=1
+c=0
+print(a)
+print(b)
+for i in range(i,j):
+    c=a+b
+    a=b
+    b=c
+    print(c)
+    
+   
+      
